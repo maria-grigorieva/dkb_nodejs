83fab2fb9051c638268c7d3000ed52db13269ad1
Maria <magsend@gmail.com>
2017-08-18 16:40:55 +0300

    dataset search route added

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,34 @@ var client = new elasticsearch.Client({
   log: 'trace'
 });
 
+
+router.get('/dataset_search', function(req, res, next) {
+	var dataset_search = {
+	  	"query": {
+	     "bool": {
+	        "must": [
+	          {"match" : {"geometry" : "ATLAS-R2-2016-00-01-00"}},
+	          {"match" : {"phys_category.keyword": "Higgs"}}
+	        ]
+	      }
+	    }
+	};
+
+	client.search({
+	  "index": "prodsys",
+	  "type": "campaign",
+	  "body": dataset_search
+	}).then(function (resp) {
+	    var hits = resp.hits.hits;
+	    // var aggs = resp.aggregations.category;
+	    console.log(hits);
+	    //console.log(aggs);
+	    res.render('dataset_search', { title: 'Express', hits: hits});
+	}, function (err) {
+	    console.trace(err.message);
+	    res.render('error', { title: 'Express'});
+	});
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
@@ -23,6 +51,14 @@ router.get('/', function(req, res, next) {
 
 	var event_summary_report = {
 	  	"size": 0,
+	  	"query": {
+	  		"bool": {
+		  		"must": { "term": { "subcampaign": "MC16a" } },
+		  		"must": { "term": { "hashtag_list": "MC16a"} },
+	  			"must": { "term": { "hashtag_list": "MC16a_CP"} },
+	  			"must": { "term": { "status": "done" } }
+	  		}
+	  	},
 		"aggs": {
 	        "category": {
 	          "terms": {"field": "phys_category.keyword"},
